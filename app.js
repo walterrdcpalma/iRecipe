4eedfd77aad38e05dc0738662985594d0a26bdf9
Walter Palma <walterrdcpalma@gmail.com>
2020-07-12 22:12:36 +0100

    form to object

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ instructions_tag.addEventListener("click", () => {
 
   recipe_cont.classList.toggle("recipe-container-toggle");
 });
+
+(function ($) {
+  $.fn.serializeFormJSON = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+      if (o[this.name]) {
+        if (!o[this.name].push) {
+          o[this.name] = [o[this.name]];
+        }
+        o[this.name].push(this.value || "");
+      } else {
+        o[this.name] = this.value || "";
+      }
+    });
+    return o;
+  };
+})(jQuery);
+
+$("form").submit(function (e) {
+  e.preventDefault();
+  var data = $(this).serializeFormJSON();
+  console.log(data);
+});
